91e15452eedd93889a7705e56bd3028a83ebe514
hikgk1 <hikgk1@users.noreply.github.com>
2016-10-26 17:51:13 +0000

    Útfæra útlit fréttasíðu og Útfæra útlit skrifaFrétt

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,33 @@ var router = express.Router();
 
 //Hérna er fyrsta route, þegar það er sótt '/' þá köllum við á fallið homePage
 router.get('/', homePage);
+router.get('/frett', frettPage);
+router.get('/skrifa', skrifaPage);
 
 //Hérna er fall, það renderar jade skjalið 'index' undir möppunni views og er með json gögn, með eina breytu title sem inniheldur "Express".
 //sbr. hvernig jade tekur json gögnin og notar þau í templating.
 function homePage(req, res, next) {
   res.render('index', { title: 'Express' });
+};
+
+//Hlutirnir sem fréttasíðan þarf
+//imgsrc er optional
+//undirfrett1-3 eru optional
+//imgsrc í undirfréttum eru optional
+function frettPage(req, res, next) {
+  res.render('frett', { rithofundur: "Jón Jónsson",
+                        undirfrett1: {url: "#", imgsrc: "banner.png", title: "Frétt 1"},
+                        undirfrett2: {url: "#", imgsrc: "banner.png", title: "Frétt 2"},
+                        undirfrett3: {url: "#", imgsrc: "banner.png", title: "Frétt 3"},
+                        dags: "20 Apríl 2016",
+                        groups: [{url: "#", text: "Íþróttir"}, {url: "#", text: "Pólitík"}],
+                        title: "Titill",
+                        imgsrc: 'banner.png',
+                        texti: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt." });
+};
+
+function skrifaPage(req, res, next) {
+  res.render('skrifa', { });
 };
 
 module.exports = router;
