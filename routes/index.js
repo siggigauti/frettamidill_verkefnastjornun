004a22c426a0a5ae07a61ca60591b6b1e2fe8684
siggigauti <siggigauti@gmail.com>
2016-11-12 16:49:34 +0000

    Alles donne?
    Virkni er komin fyrir breyta upplysingum users, login, signup, ný frétt,
    skoða flokka, sérstaka frétt. Basically allt nema admin sem átti að
    sleppa.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var article = require('../lib/articles');
+var users = require('../lib/users');
 var ensureLoggedIn = require('../middleware/ensureLoggedIn');
 
 //Hérna er fyrsta route, þegar það er sótt '/' þá köllum við á fallið homePage
@@ -9,7 +10,8 @@ router.get('/frett/:id', frettPage);
 router.get('/groups/:article_group', groupPage);
 router.get('/skrifa', ensureLoggedIn, skrifaPage);
 router.post('/skrifa', ensureLoggedIn, skrifaHandler);
-//router.get('/user', userPage);
+router.get('/user', ensureLoggedIn, userPage);
+router.post('/user', ensureLoggedIn, userHandler);
 //router.get('/admin', adminPage);
 
 
@@ -45,8 +47,7 @@ function skrifaHandler(req, res, next){
   var headline = req.body.headline;
   var content = req.body.content;
   var article_group = req.body.article_group;
-  var user = req.session.user.username;
-  console.log(user);  
+  var user = req.session.user.display_name;
   var photo = req.body.photo;
   article.createArticle(user, headline, content, article_group, photo, function(err, result){
     article.getArticleByUser(1, user, function(err, result){
@@ -93,12 +94,43 @@ function frettPage(req, res, next) {
 
 //posts er optional
 function userPage(req, res, next) {
-  res.render('user', { title: "Notendasíðan",
-                       posts: [{title: "Þetta er frétt", url: "#"},
-                               {title: "Þetta er frétt 2", url: "#"},
-                               {title: "Þetta er frétt 3", url: "#"}]
+  var user;
+  if(req.session.user){
+    user = req.session.user; 
+  }
+  article.getArticleByUser(20, user.username, function(err, result){
+    var data = {
+      title: "Notendasíða",
+      frettir: result.rows,
+      user: user
+    };
+    res.render('user', data);
   });
 };
+
+function userHandler(req, res, next){
+  var display_name = req.body.display_name;
+  var password = req.body.password;
+  var email = req.body.email;
+  var updatePass = true;
+  var user;
+  if(req.session.user){
+    user = req.session.user;
+  }
+  if(display_name.length == 0){
+    display_name = user.display_name;
+  }
+  if(password.length == 0){
+    password = user.password;
+    updatePass = false;
+  }
+  if(email.length == 0){
+    email = user.email;
+  }
+  users.updateUser(display_name, password, updatePass, email, user.id, function(err, result){
+    res.redirect('/user');
+  });
+}
 
 //frettir er optional
 function adminPage(req, res, next) {
